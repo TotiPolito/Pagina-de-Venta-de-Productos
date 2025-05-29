30fe358328932986773a0b40756b7c1a3b6fe374
Leandro Jonatan Andriani <126698066+Leandro-hub24@users.noreply.github.com>
2025-05-29 20:38:17 -0300

    Add files via upload

--- Grupo 7/js/cargar-datos.js
+++ Grupo 7/js/cargar-datos.js
@@ -13,7 +13,7 @@ linksCategorias.forEach((linkCategoria) => {
          const { Categoria, Id, Nombre, Autor, Portada, Descripcion, Rating } = item;
 
          if (linkCategoria.innerText != Categoria) return;
-         const articuloContenedor = document.querySelector("article." + Id.split("-")[1])
+         const articuloContenedor = document.getElementById("categoria00-" + Id.split("-")[1])
 
          articuloContenedor.getElementsByClassName("item-valor-nombre")[0].innerText = Nombre;
          articuloContenedor.getElementsByClassName("item-valor-autor")[0].innerText = Autor;
@@ -29,7 +29,7 @@ linksCategorias.forEach((linkCategoria) => {
             articuloContenedor.getElementsByClassName(`item-valor-personalizado_${index + 1}`)[0].innerText = item[personalizado];
          });
 
-         articuloContenedor.id = Id;
+         //articuloContenedor.id = Id;
       });
    });
 });
@@ -37,3 +37,28 @@ linksCategorias.forEach((linkCategoria) => {
 if (configuracion["modo-test-prod"] === "prod") {
    tabCategoria1.click();
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+   items.forEach((item) => {
+      const { Categoria, Id, Nombre, Autor, Portada, Descripcion, Rating } = item;
+
+      if ( "Accesorios de cabeza"!= Categoria) return;
+      const articuloContenedor = document.getElementById("categoria00-" + Id.split("-")[1])
+
+      articuloContenedor.getElementsByClassName("item-valor-nombre")[0].innerText = Nombre;
+      articuloContenedor.getElementsByClassName("item-valor-autor")[0].innerText = Autor;
+      articuloContenedor.getElementsByClassName("item-valor-portada")[0].src = Portada;
+      articuloContenedor.getElementsByClassName("item-valor-portada")[0].alt = Nombre;
+      articuloContenedor.getElementsByClassName("item-valor-descripcion")[0].innerText = Descripcion;
+      articuloContenedor.getElementsByClassName("item-valor-rating")[0].innerText = Rating;
+
+      const personalizados = Object.keys(item).filter(key => key.startsWith("personalizado_"));
+      
+      personalizados.forEach((personalizado, index) => {
+         articuloContenedor.getElementsByClassName(`item-campo-personalizado_${index + 1}`)[0].innerText = personalizado.split(".")[1];
+         articuloContenedor.getElementsByClassName(`item-valor-personalizado_${index + 1}`)[0].innerText = item[personalizado];
+      });
+
+      //articuloContenedor.id = Id;
+   });
+});
